a6a8a977b3af11470c036898b0c85935a9dc8941
João Coelho <joao.pedro.cp.coelho@gmail.com>
2016-07-26 12:32:41 +0100

    Notebook now running from start to end. moved functions to tilestools. still needs refactoring for oversampling part

--- tilestools.py
+++ tilestools.py
@@ -5,6 +5,7 @@ Tools for the TILES project
 
 """
 import numpy as np
+import pandas as pd
 
 # generate GameID
 def get_gameID(season,gametype,gamenum):
@@ -49,9 +50,9 @@ assert get_gameID(season=1999, gametype="P", gamenum=101)=='0049900101'
 assert get_gameID(season=2013, gametype="R", gamenum=1230)=='0021301230'
 
 # define PR-score
-def PRscore(y_true, y_pred):
+def F1score(y_true, y_pred):
     """
-    Compute the PR-score of y_pred
+    Compute the F1-score of y_pred
     
     Input:
         y_true (array-like): ground truth value for labels
@@ -81,10 +82,10 @@ def PRscore(y_true, y_pred):
     return prscore
 
 # quick tests
-assert np.isclose(PRscore([1, 1, 1], [1, 1, 1]), 1.)
-assert np.isclose(PRscore([1, 1, 1], [0, 0, 0]), 0.)
-assert np.isclose(PRscore([1,1,0,1], [1,0,0,0]), 0.5)
-assert np.isclose(PRscore([1,1,0,1,0,0,1,0,1], [0,1,0,1,1,1,0,0,0]), 4./9.)
+assert np.isclose(F1score([1, 1, 1], [1, 1, 1]), 1.)
+assert np.isclose(F1score([1, 1, 1], [0, 0, 0]), 0.)
+assert np.isclose(F1score([1,1,0,1], [1,0,0,0]), 0.5)
+assert np.isclose(F1score([1,1,0,1,0,0,1,0,1], [0,1,0,1,1,1,0,0,0]), 4./9.)
 
 # define event_to_OHE function
 def event_to_OHE(event_code, size):
@@ -123,3 +124,134 @@ def event_to_OHE(event_code, size):
 assert np.array_equal(event_to_OHE(4,5), np.array([0,0,0,0,1]))
 assert np.array_equal(event_to_OHE(1,10), np.array([0,1,0,0,0,0,0,0,0,0]))
 assert np.array_equal(event_to_OHE(0,2), np.array([1,0]))
+
+def build_df_events(df, features):
+    """
+    Builds a one-hot encoded dataframe from the original dataframe df and a list of relevant features
+    
+    Input:
+        df (pandas dataframe): original dataframe with all events
+        features (list): list of features to be retained
+    
+    Output:
+        df_events (pandas dataframe): one-hot encoded dataframe
+        
+    Comments:
+    
+    """
+    
+    number_of_features = len(features)
+    number_of_events = len(df)
+
+
+    df_events = pd.DataFrame(data=np.zeros((number_of_events, number_of_features)), columns = features)
+
+    # rebounds
+    home_rebound_index = df[(df.EVENTMSGTYPE==4) & (df.HOMEDESCRIPTION.notnull())].index
+    df_events.loc[home_rebound_index,"REBOUND_HOME"] = 1.0
+
+    away_rebound_index = df[(df.EVENTMSGTYPE==4) & (df.VISITORDESCRIPTION.notnull())].index
+    df_events.loc[away_rebound_index,"REBOUND_AWAY"] = 1.0
+
+    # 2PT made
+    idx = df[(df["EVENTMSGTYPE"]==1) & (df.HOMEDESCRIPTION.notnull()) & (df.HOMEDESCRIPTION.str.contains("^((?!3PT).)*$"))].index
+    df_events.loc[idx,"2PT_MADE_HOME"] = 1.0
+
+    idx = df[(df["EVENTMSGTYPE"]==1) & (df.VISITORDESCRIPTION.notnull()) & (df.VISITORDESCRIPTION.str.contains("^((?!3PT).)*$"))].index
+    df_events.loc[idx,"2PT_MADE_AWAY"] = 1.0
+
+    # 3PT made
+    idx = df[(df.EVENTMSGTYPE==1) & (df.HOMEDESCRIPTION.notnull()) & (df.HOMEDESCRIPTION.str.contains("3PT"))].index
+    df_events.loc[idx,"3PT_MADE_HOME"] = 1.0
+
+    idx = df[(df.EVENTMSGTYPE==1) & (df.VISITORDESCRIPTION.notnull()) & (df.VISITORDESCRIPTION.str.contains("3PT"))].index
+    df_events.loc[idx,"3PT_MADE_AWAY"] = 1.0
+
+    # 2PT miss
+    idx = df[(df["EVENTMSGTYPE"]==2) & (df.HOMEDESCRIPTION.str.contains("MISS")) & (df.HOMEDESCRIPTION.str.contains("^((?!3PT).)*$"))].index
+    df_events.loc[idx,"2PT_MISS_HOME"] = 1.0
+
+    idx = df[(df["EVENTMSGTYPE"]==2) & (df.VISITORDESCRIPTION.str.contains("MISS")) & (df.VISITORDESCRIPTION.str.contains("^((?!3PT).)*$"))].index
+    df_events.loc[idx,"2PT_MISS_AWAY"] = 1.0
+
+    # 3PT miss
+    idx = df[(df["EVENTMSGTYPE"]==2) & (df.HOMEDESCRIPTION.str.contains("MISS")) & (df.HOMEDESCRIPTION.str.contains("3PT"))].index
+    df_events.loc[idx,"3PT_MISS_HOME"] = 1.0
+
+    idx = df[(df["EVENTMSGTYPE"]==2) & (df.VISITORDESCRIPTION.str.contains("MISS")) & (df.VISITORDESCRIPTION.str.contains("3PT"))].index 
+    df_events.loc[idx,"3PT_MISS_AWAY"] = 1.0
+
+    # turnovers
+    idx = df[(df.EVENTMSGTYPE==5) & (df.HOMEDESCRIPTION.str.contains("Turnover"))].index
+    df_events.loc[idx,"TOV_HOME"] = 1.0
+
+    idx = df[(df.EVENTMSGTYPE==5) & (df.VISITORDESCRIPTION.str.contains("Turnover"))].index
+    df_events.loc[idx,"TOV_AWAY"] = 1.0
+
+
+    # timeouts
+    idx = df[(df.EVENTMSGTYPE==9) & (df.HOMEDESCRIPTION.notnull())].index
+    df_events.loc[idx,"TIMEOUT_HOME"] = 1.0
+
+    idx = df[(df.EVENTMSGTYPE==9) & (df.VISITORDESCRIPTION.notnull())].index
+    df_events.loc[idx,"TIMEOUT_AWAY"] = 1.0    
+
+
+    return df_events
+
+def build_cumulative_data(df_events, window_size=10):
+    """
+    Create a matrix of cumulative events 
+    
+    Input:
+        df_events (pandas dataframe): a dataframe where every row is an event
+        window_size (int): the size of the moving window
+        
+    Output:
+        X (numpy matrix): the design matrix
+        y (numpy array): the labels vector
+        
+    Comments:
+        this is used as the design matrix and corresponding labels in the case of a moving-window approach
+        the design matrix created, X, is just a sum over the previous events of whatever features df_events has
+        as a result, len(X) = len(df_events) - window_size, because for the first window_size events no cumulative data is generated
+    
+    """
+    # create design matrix X and labels y
+
+    # we define a window of size window_size and select every set of window_size points as a training example
+    # the label is whether the index of the next event is a timeout or not
+
+    # this assumes that there wasn't a timeout in the first window_size events
+
+    number_of_events, number_of_features = df_events.shape
+
+    num_training_examples = number_of_events - window_size
+
+    X = np.zeros((num_training_examples, number_of_features))
+
+    timeout_events = list(df_events[(df_events.TIMEOUT_HOME==1) | (df_events.TIMEOUT_AWAY==1) ].index)
+
+    idx=0
+    for event_id in range(window_size, number_of_events):
+        before_events = df_events.iloc[event_id-window_size : event_id]
+        X[idx,:] = before_events.sum()
+        idx+=1
+
+    # labels
+    # create list of timeout events indexes in new setting
+    # e.g. if window_size=10 and the 11th event was a timeout, then y[0] must be 1
+    timeout_events_idx = [idx-window_size for idx in timeout_events]
+
+    # create labels
+    y = np.zeros(num_training_examples)
+    y[timeout_events_idx] = 1.0
+
+
+    # remove last two columns in  X, which are the timeouts
+    X = np.delete(X, np.s_[-2:], axis=1)
+
+    # sanity check
+    assert len(X) == len(y)    
+    
+    return X, y
